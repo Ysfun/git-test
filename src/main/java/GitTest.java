a1c12548cdbd33cf75c2e8602a4e107ed5e3a609
shyunmin00 <s_hyunmin@163.com>
2021-06-16 19:56:24 +0800

    hot-fix commit2

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Git Test");
         System.out.println("hot-fix git");
+        System.out.println("************");
     }
 }
