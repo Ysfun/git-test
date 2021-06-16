b6dcb4787d9f16bb0edf9201a58dc798e2661a0f
shyunmin00 <s_hyunmin@163.com>
2021-06-16 16:45:14 +0800

    third commit

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Git Test");
         System.out.println("Git Hub (#^.^#)");
+        System.out.println("Git Hub (#^.^#)");
     }
 }
